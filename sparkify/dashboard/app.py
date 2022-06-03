9e7cbdfbeea4727d741d31b2b4c03ab524fc5ebd
Rodrigo de Alvarenga Mattos <rodrigo@bragile.com>
2022-06-03 17:40:07 -0300

    favicon fixed

--- sparkify/dashboard/app.py
+++ sparkify/dashboard/app.py
@@ -30,7 +30,7 @@ class App:
     def __config_page(self):
         st.set_page_config(
             page_title="Sparkify Dashboard",
-            page_icon="/images/favicon.ico",
+            page_icon="images/favicon.ico",
             layout="wide",
             initial_sidebar_state="expanded",
             menu_items={
